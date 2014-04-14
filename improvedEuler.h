e6ec6f34697cac24782153d7a61bc02e10093925
seenubus <asp239@cornell.edu>
2014-04-14 13:43:40 -0400

    Create improvedEuler.h

---
+++ improvedEuler.h
@@ -0,0 +1,13 @@
+//
+//  improvedEuler.h
+//  MyHannouki
+//
+//  Created by labuser on 4/14/14.
+//  Copyright (c) 2014 varnerlabs. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface improvedEuler : NSObject
+
+@end
