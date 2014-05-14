047e51665052e1c3c94631137d2d7a736dc3621b
seenubus <asp239@cornell.edu>
2014-05-14 18:19:12 -0400

    Update DataFile.h

--- DataFile.h
+++ DataFile.h
@@ -1,24 +1,27 @@
 //
 //  DataFile.h
-//  MyHannouki
+//  hannouki
 //
-//  Created by labuser on 4/14/14.
-//  Copyright (c) 2014 varnerlabs. All rights reserved.
+//  Created by Guest User on 5/14/14.
+//  Copyright (c) 2014 Guest User. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-
-
-
-@interface DataFile : NSObject {
+@interface DataFile : NSObject{
 @private
-    NSMutableArray *_rateConstantVector;
-    NSMutableArray *_initialConditionsVector;
-    NSMutableArray *_stoichiometricMatrix;
-    NSMutableArray *_measurementIndexVector;
-    NSInteger _numRateConstants;
-    NSInteger _numSpecies;
+NSMutableArray *_rateConstantVector;
+NSMutableArray *_initialConditionsVector;
+NSMutableArray *_stoichiometricMatrix;
+NSMutableArray *_measurementIndexVector;
+NSMutableArray *_parameterVector;
+    
+NSInteger _numRateConstants;
+NSInteger _numSpecies;
+NSInteger _numParameters;
+NSInteger _numStates;
+NSInteger _volumetricFlowRateIn;
+    
 }
 
 // Properties -
@@ -26,11 +29,14 @@
 @property (retain) NSMutableArray *initialConditionsVector;
 @property (retain) NSMutableArray *stoichiometricMatrix;
 @property (retain) NSMutableArray *measurementIndexVector;
+@property (retain) NSMutableArray *parameterVector;
 @property (assign) NSInteger numRateConstants;
 @property (assign) NSInteger numSpecies;
+@property (assign) NSInteger numParameters;
+@property (assign) NSInteger numStates;
+@property (assign) NSInteger volumetricFlowRateIn;
 
 // Methods -
 - (NSMutableDictionary *)constructDataStructure;
 
 @end
-
