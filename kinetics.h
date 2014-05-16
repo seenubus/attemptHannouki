a9dc4c27c9945aba854caf60efd19981e0ac5ddc
seenubus <asp239@cornell.edu>
2014-05-15 23:37:49 -0400

    Update kinetics.h

--- kinetics.h
+++ kinetics.h
@@ -1,17 +1,45 @@
+//
+//  kinetics.h
+//  hannouki
+//
+//  Created by Guest User on 5/15/14.
+//  Copyright (c) 2014 Guest User. All rights reserved.
+//
+
 #import <Foundation/Foundation.h>
 
-@interface kinetics : NSObject
-@protocol MassBalanceProtocol <NSObject>
 
-/* calculatekinetic () */
-void evaluatekinetics(register float xdot[],register float state[],register float network[][COLUMN],);
-
+@protocol kineticsProtocol <NSObject>
+void evaluateKinetics(register float rV[], register float initialConditionsVector[], register float parameterVector[]);
 @end
 
-@interface MassBalance : NSObject <MassBalanceProtocol> {
-@private
-    NSMutableArray *rateConstant;
+@interface kinetics: NSObject <kineticsProtocol>{
+    @private
+    NSMutableArray * rV;
+
+    @protected
+        double _tStart;
+        double _tStop;
+        double _tStep;
+        double _numRates;
+        float **rateConstantVector;
+        float **initalConditionsVector;
+        float **stoichiometricMatrix;
+        float **measurementIndexVecto;
+        float **parameterVector;
+        NSMutableDictionary* _dataFile;
+    
 }
 
-@property(retain) NSMutableArray *rateConstant;
+-(void) print;
+@property (assign) double tStart;
+@property (assign) double tStop;
+@property (assign) double tStep;
+@property (assign) double numRates;
+@property (retain) NSMutableDictionary *dataFile;
+
+-(void) obtainDataFileValues;
+@property (retain) NSMutableArray *rV;
+
+
 @end
