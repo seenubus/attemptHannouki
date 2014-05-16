214e4cbbd83d6f6e43815aafae34651ed831d27c
seenubus <asp239@cornell.edu>
2014-05-15 23:36:56 -0400

    Update BalanceEquations.m

--- BalanceEquations.m
+++ BalanceEquations.m
@@ -1,75 +1,86 @@
-#import "MassBalance.h"
+//
+//  BalanceEqns.m
+//  hannouki
+//
+//  Created by Guest User on 5/15/14.
+//  Copyright (c) 2014 Guest User. All rights reserved.
+//
 
-#define COLUMN 1234
-#define ROW 1234
+#import "BalanceEqns.h"
+#import "kinetics.h"
+#import "computeInputFlowRate"
 
-@implementation BalanceEquations
+@implementation BalanceEqns
+@synthesize dxdt;
+@synthesize numRates = _numRates;
+@synthesize dataFile = _dataFile;
 
-@synthesize rateConstant;
-
-- (id)init {
-    self = [super init];
-    if (self) {
-        
-        // Code to initilize the object -
+-(id) init{ self[super init];
+    if (self){NSMutableArray *dxdt[[NSMutableARray alloc] init];
+        NSNumber *DXDT = [NSNumber numberWithFloat:0.2f];
+        [dxdt addObject: DXDT];
     }
     return self;
 }
 
-// trying to compute Kinetics to get qV like in the Matlab Code//
-
-
- - (NSMutableArray *) calculateKinetics
- {
- NSMutableArray *kinetic;
- 
- [kinetic addObject:(self.species * self.rateConstant)];
- 
- return kinetic; 
- 
- }
- */
-
-
-
-void evaluateBalanceEquations( register float dxdt[], register float x, register float StoichMat [], float kV[], float qV[])
+//Do I have to obtainDataFileValues again???
+-(void) obtainDataFileValues
 {
-
-
-
-    /* inclde call kinetics */
+    //how to store float type objects in these vectors/arrays???
+    //that's what i'm trying to do here...
     
-    int row, column;
+    rateConstantVector = [self.dataFile valueForKey:@"RateConstantVector"];
+    NSNumber *num = [NSNumber numberWithFloat:0.2f];
+    [rateConstantVector addObject:num];
     
-    column = row = 0;
-    while(row < ROW){
-        while (column < COLUMN){
-			xdot[row] += kinetic[column] * network[row][column]; 
-            column++;
-        }
-        if (column == COLUMN)
-            column = 0;
-        row++;
-    }
+    initialConditionsVector = [self.dataFile valueForKey:@"InitialConditionsVector"];
+    NSNumber *fum = [NSNumber numberWithFloat:0.2f];
+    [initialConditionsVector addObject:fum];
+    
+    parameterVector = [self.dataFile valueForKey:"ParameterVector"];
+    NSNumber *gum = [NSNumber numberWithFloat:0.2f];
+    [parameterVector addObject:gum];
     
     
-float ALPHA, XIN, SIN, PIN, YXS, YXP, BETA
-
-ALPHA = kV[0];
-XIN = kV[1];
-SIN = kV[2];
-PIN = kV[3];
-YXS = kV[12];
-YXP = kV[13];
-BETA = kV[14];
-/ Mass balances /
-	dxdt[0] = (FIN/V)*(XIN - X) + (qV[0] - qV[1])*X;							          /cell mass
-	dxdt[1] = (FIN/V)*(SIN - S) - (1/YXS)*(qV[2])*X + (BETA/YXS)*qV[1]*X;		/substrate
-	dxdt[2] = (FIN/V)*(PIN - P) + (1/YXP)*qV[3]*X;								          /product
-	dxdt[3] = FIN - ALPHA*FIN; 													                    /volume
-	
 }
-	
-	
+
+
+void EvaluateBalanceEqns (register float dxdt[], register float initialConditionsVector[], register float parameterVector[])
+{
+    float rV;
+    float FIN, X, S, P, V, ALPHA, XIN,SIN, PIN, YXS, YXP, BETA;
+    
+    evaluateKinetics ( register float rV[], register float initialConditionsVector[], register float parameterVector[]);
+
+    
+  FIN = ComputeInputFlowRate( );
+  
+    
+    X = initialConditionsVector[0];
+    S = initialConditionsVector[1];
+    P = initialConditionsVector[2];
+    V = initialConditionsVector[3];
+                          
+    ALPHA = parameterVector [0];
+    XIN = parameterVector [1];
+    SIN = parameterVector [2];
+    PIN = parameterVector [3];
+    YXS = parameterVector [10];
+    YXP = parameterVector [11];
+    BETA = parameterVector[12];
+ 
+    //CellMass
+    dxdt[0] = (FIN/V)*(XIN-X) + (rV[0] - rV[1])*X;
+    //Substrate
+    dxdt[1] = (FIN/V)*(SIN-S) + (1/YXS)*(rV[2])*X + (BETA/YXS)*rV[1]*X;
+    //Product
+    dxdt[2] = (FIN/V)*(PIN-P) + (1/YXP)*rV[3]*X;
+    //volume
+    dxdt[3] = FIN - ALPHA*FIN;
+    
+    
+    
+    return;
+                      }
+
 @end
-	
